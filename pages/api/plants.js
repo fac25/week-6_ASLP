63caf1484377582abc723a9695627f3af494d47f
Abbbyccc <abbychien@hotmail.com>
2022-10-26 10:47:56 +0100

    set up an api page for data fetch

---
+++ pages/api/plants.js
@@ -0,0 +1,9 @@
+const { PrismaClient } = require ('@prisma/client')
+
+const prisma = new PrismaClient()
+
+export default async function handler(req, res) {
+  const plants = await prisma.plant.findMany()
+
+  res.status(200).json(plants)
+}

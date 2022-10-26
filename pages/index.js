c02dd5be4dd539cb8e3ca85a80f70254802de52b
Abbbyccc <abbychien@hotmail.com>
2022-10-26 14:09:34 +0100

    retrieve data Co-authored-by: Lisa <lisahns@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,35 @@ import Link from 'next/link'
 import utilStyles from '../styles/utils.module.css'
 import Layout, { siteTitle } from '../components/layout'
 
+import useSWR from 'swr'
 
+const fetcher =  (...args) =>  fetch(...args).then((res) => res.json())
+
+function Plant() {
+const { data, error} = useSWR('/api/plants', fetcher)
+  if (error) return <div>Failed to load</div>
+  if (!data) return <div>Loading...</div>
+
+  return (
+    <ul>    
+    {data.map((obj)=>(
+      <li key={obj.id}>
+        <p>{obj.name}</p>  
+        <p>{obj.price}</p>  
+        <p>{obj.description}</p> 
+      </li>
+    ) )
+  }
+  </ul>
+  )
+}
 export default function Home() {
     return (
         <Layout home>
             <Head>
                 <title>{siteTitle}</title>
             </Head>
+            <Plant />
             <div className={utilStyles.grid}>
                 <div className={utilStyles.card}>
                     <Image
@@ -26,7 +48,7 @@ export default function Home() {
                         <span>£30</span>
                     </div>
                 </div>
-
+               
                 <div className={utilStyles.card}>
                     <Image
                         priority

625b11fa2297d8aea2c9db11c1ba63ca8fc14324
mariapaz <mariapazgar@gmail.com>
2022-10-26 11:43:00 +0100

    home-layout

---
+++ components/layout.js
@@ -0,0 +1,41 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import Nav from '../components/nav'
+
+export const siteTitle = 'Planty'
+
+export default function Layout({ children, home }) {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                    name="description"
+                    content="On-line shop for the best interior pot plants"
+                />
+                <meta
+                    property="og:image"
+                    content={`https://og-image.vercel.app/${encodeURI(
+                        siteTitle
+                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                />
+                <meta name="og:title" content={siteTitle} />
+                <meta name="twitter:card" content="summary_large_image" />
+            </Head>
+            <header className={styles.header}>
+                <Nav />
+            </header>
+            <main>{children}</main>
+            {!home && (
+                <div className={styles.backToHome}>
+                    <Link href="/">
+                        <a>← Back to home</a>
+                    </Link>
+                </div>
+            )}
+        </div>
+    )
+}

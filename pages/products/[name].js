98e0f7acfb1f2df2824f3ae06f24e46b2bd89fe1
Paz <mariapazgar@gmail.com>
2022-10-27 12:06:05 +0100

    fetch static product data
    Co-authored-by: Abby <hello@abbychien.com>
    Co-authored-by: Lisa <lisahns@users.noreply.github.com>
    Co-authored-by: Suraj <SurPun@users.noreply.github.com>

--- pages/products/[name].js
+++ pages/products/[name].js
@@ -1,40 +1,44 @@
-import prisma from '../api/paths'
-import { makeSerializable } from '../../lib/utils'
+// import prisma from '../../lib/prisma.js'
+// import { makeSerializable } from '../../lib/utils'
 
-const Plant = (props) => {
-    let plantName = props.name
-    return <h1>{plantName}</h1>
+// export const getServerSideProps = async (context) => {
+//     const plant = await prisma.plant.findUnique({
+//         where: { name: String(context.params.name) },
+//     })
+//     console.log(plant)
+//     return { props: { ...makeSerializable(plant) } }
+// }
+
+// export all the Paths that we have
+
+export async function getStaticPaths() {
+    const res = await fetch('http://localhost:3000/api/plants')
+    const plants = await res.json()
+
+    const path = plants.map((plant) => ({
+        params: { name: String(plant.name) },
+    }))
+
+    return {
+        paths: path,
+        fallback: false,
+    }
 }
-export const getServerSideProps = async (context) => {
-    const plant = await prisma.plant.findUnique({
-        where: { name: String(context.params.name) },
-    })
-    console.log(plant)
-    return { props: { ...makeSerializable(plant) } }
+
+export async function getStaticProps({ params }) {
+    const res = await fetch('http://localhost:3000/api/plants/')
+
+    const plants = await res.json()
+    const filterplants = plants.filter(
+        (plant) => plant.name.toString() === params.name
+    )
+
+    return {
+        props: filterplants[0],
+        revalidate: 10,
+    }
 }
-
-export default Plant
-
-// export async function getStaticPaths() {
-//     const plants = await routeHandler()
-
-//     const paths = plants.map((plant) => ({
-//         params: { name: String(plant.name) },
-//     }))
-
-//     return {
-//         paths,
-//         fallback: false,
-//     }
-// }
-
-// export async function getStaticProps({ params }) {
-//     return {
-//         props: {},
-//         revalidate: 10,
-//     }
-// }
-
-// export default function Post() {
-//     return <h1>Hello!</h1>
-// }
+export default function Plant(props) {
+    //
+    return <h1>{props.name}</h1>
+}

1ca8a4ab0325c875d3e89cd240df3ce09103284c
Paz <mariapazgar@gmail.com>
2022-10-27 14:48:16 +0100

    Added vew product btn Co-authored-by: Abby <hello@abbychien.com> Co-authored-by: Lisa <lisahns@users.noreply.github.com> Co-authored-by: Suraj <SurPun@users.noreply.github.com>

--- components/products.js
+++ components/products.js
@@ -1,33 +1,43 @@
 import Image from 'next/image'
-import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
 
+import utilStyles from '../styles/utils.module.css'
 import useSWR from 'swr'
 
-const fetcher =  (...args) =>  fetch(...args).then((res) => res.json())
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
 export default function Products() {
-const { data, error} = useSWR('/api/plants', fetcher)
-  if (error) return <div>Failed to load</div>
-  if (!data) return <div>Loading...</div>
+    const { data, error } = useSWR('/api/plants', fetcher)
+    if (error) return <div>Failed to load</div>
+    if (!data) return <div>Loading...</div>
 
-  return (
-    <>
-    {data.map((obj)=>(
-    <div className={utilStyles.card} key={obj.id}>
-            <Image
-                priority
-                src={obj.image_link}
-                className={utilStyles.cardImg}
-                height={144}
-                width={144}
-                alt=""      
-            />
-            <div className={utilStyles.cardDetails}>
-                <h2>{obj.name}</h2>
-                <span>£{obj.price}</span>
-             </div>
-     </div>
-    ))}
-     </>
-  )
+    return (
+        <>
+            {data.map((obj) => (
+                <div className={utilStyles.card} key={obj.id}>
+                    <Image
+                        priority
+                        src={obj.image_link}
+                        className={utilStyles.cardImg}
+                        height={144}
+                        width={144}
+                        alt=""
+                    />
+                    <div className={utilStyles.cardDetails}>
+                        <h2>{obj.name}</h2>
+                        <span>£{obj.price}</span>
+                        <addToBasketBtn />
+                        <Link
+                            href={
+                                'products/' +
+                                obj.name.toLowerCase().replace(' ', '-')
+                            }
+                        >
+                            View product
+                        </Link>
+                    </div>
+                </div>
+            ))}
+        </>
+    )
 }

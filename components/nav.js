b6c77e821b1954d1d141e95bb077476273476e7f
lisahns <lisa.hanes@hotmail.de>
2022-10-28 12:09:53 +0100

    trying to imply global state

--- components/nav.js
+++ components/nav.js
@@ -1,14 +1,19 @@
 import Link from 'next/link'
 import utilStyles from '../styles/utils.module.css'
 import CheckOutBtn from './checkoutBtn'
+import React, { useContext } from 'react'
+import { globalStateContext } from '../pages/_app'
 //import Image from "next/image";
 export default function Nav() {
+    const amount = useContext(globalStateContext)
+    console.log(amount)
     return (
         <nav className={utilStyles.mainNav}>
             <Link href="/">
                 <a className={utilStyles.logo}>Planty</a>
             </Link>
             <CheckOutBtn />
+            <span>{amount}</span>
         </nav>
     )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,23 @@ import '../styles/globals.css'
 import React, { useState } from 'react'
 
 export const globalStateContext = React.createContext()
-// const dispatchStateContext = React.createContext(undefined)
 
 function MyApp({ Component, pageProps }) {
     const [amount, setAmount] = useState(0)
+
+    function addToCart() {
+        const basket = localStorage.getItem('basket')
+        const array = JSON.parse(basket)
+
+        const sum = array
+            .map((element) => element.quantities)
+            .reduce((a, b) => a + b, 0)
+        setAmount((amount = sum))
+    }
     return (
         <globalStateContext.Provider value={amount}>
             <Component {...pageProps} />
+            {/* <button onClick={addToCart}>add to cart test</button> */}
         </globalStateContext.Provider>
     )
 }
